914a99f4a6cdc80bbde72e41c8eb30f996472d6b
jiraya2022 <masterjiraya2022@gmail.com>
2024-07-17 15:13:45 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", "24579842"))
     API_HASH = os.environ.get("API_HASH", "ec6105bf1a02c98f837300546dc341d1")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7156606751:AAE1syKZmWjaQecu5gb0HqFeXuT01wSRgzg")
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "memory")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002111303673"))
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mogodb001:mogodb001@cluster0.ewge2ru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "1991522624").split(" ")]
